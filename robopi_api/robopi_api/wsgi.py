a67be852b2552003ad243bee7d2e662ce6e15c08
Tengda Yan <teyan@microsoft.com>
2018-04-29 15:16:07 +0800

    start project

---
+++ robopi_api/robopi_api/wsgi.py
@@ -0,0 +1,19 @@
+"""
+WSGI config for robopi_api project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
+"""
+
+import os,sys
+
+from django.core.wsgi import get_wsgi_application
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../../")))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../")))
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "robopi_api.settings")
+
+application = get_wsgi_application()
