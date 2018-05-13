104afb44d9dcb98b7dd11fe447864ba7b6417f4f
Tengda Yan <teyan@microsoft.com>
2018-05-13 13:37:07 +0800

    Angular

---
+++ html/libs/js/sensor.js
@@ -0,0 +1,31 @@
+/// <reference path="angular.min.js"/>"
+
+var model ={
+     name: "RoboPi",
+     accel:  "0,0,0",
+     gyro: "0,0,0",
+     temp: 0,
+}
+
+var sensorApp = angular.module("sensorApp", []);
+
+console.log("Started");
+sensorApp.controller("SensorCtrl", function($scope, $http){
+    loadData($scope, $http);
+    $scope.sensor = model;
+    $scope.fetchData = function(){loadData($scope, $http)};
+});
+
+function loadData($scope, $http) {
+    console.log("fetchData");
+    $http.get("api/sensor/accelerator/")
+    .then(function(data){
+        $scope.sensor.accel = data.data;
+    });
+    $http.get("api/sensor/gyro/")
+    .then(function(data){
+        $scope.sensor.gyro = data.data;
+    });
+    model.temp += 1;
+    
+}
